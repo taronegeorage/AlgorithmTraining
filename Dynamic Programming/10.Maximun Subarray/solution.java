216fef57da9e20d0a7a4083ad8f6a132eb859d26
ZHAO, Jiaqi <603875085@qq.com>
2019-03-06 23:28:21 +0800

    Update solution.java

--- Dynamic Programming/10.Maximun Subarray/solution.java
+++ Dynamic Programming/10.Maximun Subarray/solution.java
@@ -16,6 +16,32 @@ class Solution {
 }
 
 /*
+    // time : O(n) space : O(n);
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = nums[i] + (dp[i - 1] < 0 ? 0 : dp[i - 1]);
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    // time : O(n) space : O(1);
+    public int maxSubArray2(int[] nums) {
+        int res = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(nums[i], sum + nums[i]);
+            res = Math.max(res, sum);
+        }
+        return res;
+    }
+*/
+
+
+/*
     public int maxSubArray(int[] nums) {
         int[] presum = new int[nums.length+1];
         int sum = 0;
